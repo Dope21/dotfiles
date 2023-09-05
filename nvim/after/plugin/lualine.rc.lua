19fa9296e026adee14f62bd4d7a74f71f4f2d52b
Dope21 <thanasaklimsila@gmail.com>
2023-09-05 20:01:09 +0700

    update

--- nvim/after/plugin/lualine.rc.lua
+++ nvim/after/plugin/lualine.rc.lua
@@ -1,7 +1,7 @@
 local status, lualine = pcall(require, "lualine")
-if (not status) then 
-	print("hi")
-	return
+if (not status) then
+  print("hi")
+  return
 end
 
 lualine.setup {
@@ -18,11 +18,19 @@ lualine.setup {
     lualine_c = { {
       'filename',
       file_status = true, -- displays file status (readonly status, modified status)
-      path = 0 -- 0 = just filename, 1 = relative path, 2 = absolute path
+      path = 0            -- 0 = just filename, 1 = relative path, 2 = absolute path
     } },
     lualine_x = {
-      { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
-        hint = ' ' } },
+      {
+        'diagnostics',
+        sources = { "nvim_diagnostic" },
+        symbols = {
+          error = ' ',
+          warn = ' ',
+          info = ' ',
+          hint = ' '
+        }
+      },
       'encoding',
       'filetype'
     },
@@ -35,7 +43,7 @@ lualine.setup {
     lualine_c = { {
       'filename',
       file_status = true, -- displays file status (readonly status, modified status)
-      path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+      path = 1            -- 0 = just filename, 1 = relative path, 2 = absolute path
     } },
     lualine_x = { 'location' },
     lualine_y = {},
