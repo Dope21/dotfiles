a0613bf9545ae068a024464582bb0accf65ec0aa
Dope21 <thanasaklimsila@gmail.com>
2023-11-23 14:24:13 +0700

    install lazyvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,2 @@
-require('highlights')
-require('plugins')
-require('base')
-require('mapping')
-
--- copy to clip board for WSL
-vim.cmd [[
-  augroup Yank
-  autocmd!
-  autocmd TextYankPost * :call system('/mnt/c/windows/system32/clip.exe ',@")
-  augroup END
-]]
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
