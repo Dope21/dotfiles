23840d020527af2a4ea42da4a52dace9ad43fb88
Dope21 <86032673+Dope21@users.noreply.github.com>
2024-06-06 17:17:40 +0700

    feat(fish): alias nvim -> vim

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,4 @@ set fish_greeting ""
 
 set -gx PATH /opt/homebrew/bin $PATH
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+command -qv nvim && alias vim nvim
