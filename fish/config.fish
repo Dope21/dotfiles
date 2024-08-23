541b6daba9c48b1dfb346d7dc68dd0eb4202412f
dope21 <thanasaklimsila@gmail.com>
2024-08-23 15:03:59 +0700

    feat: linuxbrew path

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ set fish_greeting ""
 
 set -gx PATH /opt/homebrew/bin $PATH
 set -gx PATH /usr/local/bin $PATH
+set -gx PATH /home/linuxbrew/.linuxbrew/bin $PATH
 
 # alias
 command -qv nvim && alias vim nvim
