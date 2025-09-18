dc2c5bf085fe0ee1c97aca68c984a581ca2b71b9
Dope21 <thanasaklimsila@gmail.com>
2025-09-18 22:28:20 +0700

    feat(powershell): profile init

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,13 @@
+Clear-Host
+Write-Host "Well cum!!, $env:USERNAME!"
 
 Set-Alias g git
 Set-Alias vim nvim
 
+Import-Module -Name Terminal-Icons
+
+Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function ForwardWord
+Set-PSReadLineKeyHandler -Chord "Ctrl-l" -Function AcceptSuggestion
+
 $theme = Join-Path (Split-Path $PROFILE) "oh-my-posh-theme.omp.json"
 oh-my-posh init pwsh --config $theme | Invoke-Expression
-
