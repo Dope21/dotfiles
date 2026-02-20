5662f50062988f26267ebdfe5b9b5e504ce02ce1
Dope21 <86032673+Dope21@users.noreply.github.com>
2026-02-20 17:01:31 +0700

    feat: basic read config

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"fmt"
+	"log"
 
 	"gopkg.in/yaml.v3"
 )
@@ -28,5 +29,13 @@ type LinkMap struct {
 }
 
 func main() {
+	data, err := os.ReadFile("./template.yaml")
+	if err != nil {
+		log.Fatal("error")
+	}
 
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+
+	fmt.Printf("YAML data: %+v\n", config)
 }
